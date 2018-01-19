565e5cd493c6bb5383505e1d0ea9f5d8a3ae6b24
Andrew Moore <andwilmo@umich.edu>
2018-01-19 17:40:01 -0500

    Update gather-ojjdp-jj-crime-rates.R
    - perform some minor cleaning on the column names
    - drop the rows with metadata
    - rename output files to reflect that the data reflect arrests
    - minor corrections
    
    - add plot
    - add data

--- R/gather-ojjdp-jj-crime-rates.R
+++ R/gather-ojjdp-jj-crime-rates.R
@@ -5,7 +5,7 @@ library(scales)
 
 # the online statistical briefing book has data from 2000 to 2016
 yrs  <- 2000:2016
-fils <- glue("https://www.ojjdp.gov/ojstatbb/crime/qa05103.asp?qaDate={yr}&export=yes")
+fils <- glue("https://www.ojjdp.gov/ojstatbb/crime/qa05103.asp?qaDate={yrs}&export=yes")
 
 # helper to do some minor formatting/cleaning of each file
 crime_getter <- function(link) {
@@ -26,15 +26,23 @@ jjc <- map_df(fils, function(link) {
   crime_getter(link)
 })
 
+# minor cleanup of names/cols
+jjc <- jjc %>%
+  rename(state = State, reporting_coverage = `Reporting Coverage`) %>%
+  mutate(reporting_coverage = as.numeric(str_replace(reporting_coverage, "%", "")))
+
+# there's some footer information in each year's file-- will drop
+jjc <- filter(jjc, nchar(state) < 30)
+
 # plot the data for michigan
 p0 <- jjc %>%
-  filter(State == "Michigan") %>%
+  filter(state == "Michigan") %>%
   ggplot(aes(x = year, y = rate, color = index)) +
   geom_line() +
   geom_point() +
   scale_color_manual(values = viridis(7)) +
   scale_y_continuous(labels = comma) +
-  theme_minimal() +
+  theme_minimal(base_size = 12) +
   theme(
     legend.position = c(.8, .8),
     legend.title = element_blank()
@@ -42,14 +50,14 @@ p0 <- jjc %>%
   labs(
     x = "",
     y = "Rate per 100,000 persons between the ages of 10-17",
-    title = "Juvenile Crime Rates in Michigan, 2000 - 2016",
+    title = "Juvenile Arrest Rates in Michigan, 2000 - 2016",
     caption = "Source: OJJDP Statistical Briefing Book (Available Online)"
   )
 
 p0
 
 # save the plot
-ggsave(p0, "jj-rates-mi-00-16.png")
+ggsave("jj-arrest-rates-mi-00-16.png", p0)
 
 # save the data
-write_csv(jjc, "data/ojjdp-jj-crime-rates.csv")
+write_csv(jjc, "data/ojjdp-jj-arrest-rates-00-16.csv")
